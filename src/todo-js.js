570da4203bdcfaddc9979b49a28d0ee88030bd67
sasuga-sato <117708142+sasuga-sato@users.noreply.github.com>
2022-11-10 17:33:00 +0900

    修正

--- src/todo-js.js
+++ src/todo-js.js
@@ -1,1 +1,10 @@
-import "./style.css";
+import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する。
+  alert();
+};
+
+document
+  .getElementById("add-button") //ID取得
+  .addEventListener("click", () => onClickAdd()); //クリックした際に関数実行
